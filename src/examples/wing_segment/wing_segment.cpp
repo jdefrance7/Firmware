e08b47c5d8bb7d8de1336192501d1ec00e8e7a19
jdefrance7 <defra022@umn.edu>
2020-06-08 13:58:54 -0500

    Updated Wing Segment example module.

--- src/examples/wing_segment/wing_segment.cpp
+++ src/examples/wing_segment/wing_segment.cpp
@@ -34,7 +34,7 @@
 /**
  *	@file wing_segment.cpp
  *
- *	Example of winglet uORB topic subscription and printing.
+ *	Example of wing segment uORB topic subscription and printing.
  *
  * 	@author Joe DeFrance
  */
@@ -51,114 +51,136 @@
 #include <uORB/uORB.h>
 #include <uORB/topics/sensor_wing_segment.h>
 
-extern "C" __EXPORT int wing_segment_main(int argc, char *argv[]);
+void print_usage()
+{
+	PX4_INFO("wing_segment [ info | status | run ]\n");
+	return;
+}
 
+void print_info()
+{
+	PX4_INFO("Wing segment module for printing the orientation of IMU sensors.\n");
+	return;
+}
+
+extern "C" __EXPORT int wing_segment_main(int argc, char *argv[]);
 int wing_segment_main(int argc, char *argv[])
 {
-	PX4_INFO("Entering Wing Segment Main...");
-
-	// PX4_INFO("Argv:");
-	// for (int i = 0; i < argc; ++i)
-	// {
-	// 	PX4_INFO("  %d: %s", i, argv[i]);
-	// }
-
-	int sensor_wing_segment_fd[4];
-
-	px4_pollfd_struct_t wing_segment[4];
-
-	for(int n = 0; n < 4; n++)
+	if(argc == 1)
 	{
-		sensor_wing_segment_fd[n] = orb_subscribe_multi(ORB_ID(sensor_wing_segment), n);
-		wing_segment[n] = {.fd = sensor_wing_segment_fd[n], .events = POLLIN};
+		print_usage();
+		return 0;
 	}
 
-	int error_counter = 0;
-
-	float xyzw[4][4];
-
-	// Print 5 instances
-	for (int instance = 0; instance < 5; instance++)
+	if(!strcmp(argv[1], "info"))
 	{
-		for(int channel = 0; channel < 4; channel++)
-		{
-			/* wait for sensor update of 1 file descriptor for 1000 ms (1 second) */
-			int poll_ret = px4_poll(&(wing_segment[channel]), 1, 1000);
-
-			/* handle the poll result */
-			if (poll_ret == 0)
-			{
-				/* this means none of our providers is giving us data */
-				PX4_ERR("Got no data from winglet %d within a second", channel);
-			}
-			else if (poll_ret < 0)
-			{
-				/* this is seriously bad - should be an emergency */
-				if (error_counter < 10 || error_counter % 50 == 0)
-				{
-					/* use a counter to prevent flooding (and slowing us down) */
-					PX4_ERR("ERROR return value from poll(): %d", poll_ret);
-				}
-				error_counter++;
-
-			}
-			else
-			{
-
-				if(wing_segment[channel].revents & POLLIN)
-				{
-					/* obtained data for the file descriptor */
-					struct sensor_wing_segment_s wing_segment_msg;
-
-					/* copy sensors raw data into local buffer */
-					orb_copy(ORB_ID(sensor_wing_segment), sensor_wing_segment_fd[channel], &wing_segment_msg);
-
-					/* print faw data */
-					PX4_INFO("\nChannel %d:\n  Timestamp: %llu\n  ID: %d\n  CAL: %d\n  X: %f\n  Y: %f\n  Z: %f\n  W: %f\n",
-						channel,
-						wing_segment_msg.timestamp,
-						wing_segment_msg.id,
-						wing_segment_msg.callibration,
-						(double)wing_segment_msg.x,
-						(double)wing_segment_msg.y,
-						(double)wing_segment_msg.z,
-						(double)wing_segment_msg.w
-					);
-
-					/* store raw data according to id */
-					xyzw[wing_segment_msg.id][0] = wing_segment_msg.x;
-					xyzw[wing_segment_msg.id][1] = wing_segment_msg.y;
-					xyzw[wing_segment_msg.id][2] = wing_segment_msg.z;
-					xyzw[wing_segment_msg.id][3] = wing_segment_msg.w;
-				}
-			}
-		}
-
-		// Calculate quaternion dot product
-		double dots[] = {0, 0, 0, 0};
-		for(int joint = 0; joint < 4; joint++)
-		{
-			for(int k = 0; k < 4; k++)
-			{
-				dots[joint] += (double)(xyzw[joint][k]*xyzw[(joint+1)%4][k]);
-			}
-		}
-
-		// Compute angles between adjacent unit quaternions
-		double angles[4];
-		for(int joint = 0; joint < 4; joint++)
-		{
-			angles[joint] = 2 * acos(dots[joint]) / 3.14 * 180;
-		}
-
-		// Print angles
-		for(int joint = 0; joint < 4; joint++)
-		{
-			PX4_INFO("Angle %d: %f deg", joint, angles[joint]);
-		}
+		print_info();
+		return 0;
 	}
 
-	PX4_INFO("Exiting Wing Segment Main...");
+	else if(!strcmp(argv[1], "status"))
+	{
+		PX4_INFO("TODO: print number of wing segments connected and their status\n");
+		return 0;
+	}
 
+	else if(!strcmp(argv[1], "run"))
+	{
+		int sensor_wing_segment_fd[4];
+
+		px4_pollfd_struct_t wing_segment[4];
+
+		for(int n = 0; n < 4; n++)
+		{
+			sensor_wing_segment_fd[n] = orb_subscribe_multi(ORB_ID(sensor_wing_segment), n);
+			wing_segment[n] = {.fd = sensor_wing_segment_fd[n], .events = POLLIN};
+		}
+
+		int error_counter = 0;
+
+		float xyzw[4][4];
+
+		// Print 5 instances
+		for (int instance = 0; instance < 5; instance++)
+		{
+			for(int channel = 0; channel < 4; channel++)
+			{
+				/* wait for sensor update of 1 file descriptor for 1000 ms (1 second) */
+				int poll_ret = px4_poll(&(wing_segment[channel]), 1, 1000);
+
+				/* handle the poll result */
+				if (poll_ret == 0)
+				{
+					/* this means none of our providers is giving us data */
+					PX4_ERR("Got no data from wing segment %d within a second", channel);
+				}
+				else if (poll_ret < 0)
+				{
+					/* this is seriously bad - should be an emergency */
+					if (error_counter < 10 || error_counter % 50 == 0)
+					{
+						/* use a counter to prevent flooding (and slowing us down) */
+						PX4_ERR("ERROR return value from poll(): %d", poll_ret);
+					}
+					error_counter++;
+
+				}
+				else
+				{
+
+					if(wing_segment[channel].revents & POLLIN)
+					{
+						/* obtained data for the file descriptor */
+						struct sensor_wing_segment_s wing_segment_msg;
+
+						/* copy sensors raw data into local buffer */
+						orb_copy(ORB_ID(sensor_wing_segment), sensor_wing_segment_fd[channel], &wing_segment_msg);
+
+						/* print faw data */
+						PX4_INFO("\nChannel %d:\n  Timestamp: %llu\n  ID: %d\n  CAL: %d\n  X: %f\n  Y: %f\n  Z: %f\n  W: %f\n",
+							channel,
+							wing_segment_msg.timestamp,
+							wing_segment_msg.id,
+							wing_segment_msg.callibration,
+							(double)wing_segment_msg.x,
+							(double)wing_segment_msg.y,
+							(double)wing_segment_msg.z,
+							(double)wing_segment_msg.w
+						);
+
+						/* store raw data according to id */
+						xyzw[wing_segment_msg.id][0] = wing_segment_msg.x;
+						xyzw[wing_segment_msg.id][1] = wing_segment_msg.y;
+						xyzw[wing_segment_msg.id][2] = wing_segment_msg.z;
+						xyzw[wing_segment_msg.id][3] = wing_segment_msg.w;
+					}
+				}
+			}
+
+			// Calculate quaternion dot product
+			double dots[] = {0, 0, 0, 0};
+			for(int joint = 0; joint < 4; joint++)
+			{
+				for(int k = 0; k < 4; k++)
+				{
+					dots[joint] += (double)(xyzw[joint][k]*xyzw[(joint+1)%4][k]);
+				}
+			}
+
+			// Compute angles between adjacent unit quaternions
+			double angles[4];
+			for(int joint = 0; joint < 4; joint++)
+			{
+				angles[joint] = 2 * acos(dots[joint]) / 3.14 * 180;
+			}
+
+			// Print angles
+			for(int joint = 0; joint < 4; joint++)
+			{
+				PX4_INFO("Angle %d: %f deg", joint, angles[joint]);
+			}
+		}
+		return 0;
+	}
 	return 0;
 }
